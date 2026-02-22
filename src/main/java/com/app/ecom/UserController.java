3df2df905b32d59bfd130fd5e071e41e64717395
JagdeepSingh13 <jagdeeps3105@gmail.com>
2026-02-22 23:38:00 +0530

    rest basics

--- src/main/java/com/app/ecom/UserController.java
+++ src/main/java/com/app/ecom/UserController.java
@@ -1,19 +1,48 @@
 package com.app.ecom;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
-    private List<User> userList = new ArrayList<>();
+    private final UserService userService;
 
     @GetMapping("/api/users")
-    public List<User> getAllUsers() {
-        return userList;
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        return ResponseEntity.ok(userService.fetchAllUsers());
+    }
+
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+//        User user = userService.fetchUser(id);
+//        if(user == null)
+//            return ResponseEntity.notFound().build();
+//        return ResponseEntity.ok(user);
+
+        return userService.fetchUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/api/users")
+    public List<User> createUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PutMapping("/api/users/{id}")
+    public ResponseEntity<String> createUser(@PathVariable Long id, @RequestBody User user) {
+        boolean updated = userService.updateUser(id, user);
+        if(updated)
+            return ResponseEntity.ok("user updated");
+        return ResponseEntity.notFound().build();
     }
 
 }
